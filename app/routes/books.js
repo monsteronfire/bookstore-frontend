058ebabdaca2feec5b8658a140d0346fec3f40db
monsteronfire <monsteronfire@gmail.com>
2016-09-20 08:05:15 +0800

    Show books on home page
    * Also add book-cover component
    * Also add CSS
    * Also set model in books to be 'books'

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('book');
+  },
+
+  setupController(controller, model) {
+    Ember.set(controller, 'books', model);
+  }
 });
