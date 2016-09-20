be7fabea1884c5db284a5f7edf56ad1b9ed02968
monsteronfire <monsteronfire@gmail.com>
2016-09-20 09:08:02 +0800

    Add most of the author's profile page
    * Need to figure out why author's books aren't showing up

---
+++ app/routes/author.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('author', params.author_id);
+  },
+
+  setupController(controller, model) {
+    Ember.set(controller, 'author', model);
+  }
+});
