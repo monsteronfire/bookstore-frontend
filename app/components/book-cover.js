a8c5e9316017f7fcfd391e228a3d9b38ff744821
monsteronfire <monsteronfire@gmail.com>
2016-09-20 08:17:32 +0800

    Add purchase modal-dialog
    * Also add 'open' and 'close' actions to the dialog

--- app/components/book-cover.js
+++ app/components/book-cover.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    open() {
+      Ember.set(this, 'isShowingModal', true);
+    },
+
+    close() {
+      Ember.set(this, 'isShowingModal', false);
+    }
+  }
 });
